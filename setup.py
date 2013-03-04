38b42fc132e5b5a0212085a141e5e8d27e8182e4
Michal Horejsek <horejsekmichal@gmail.com>
2013-03-04 16:22:39 +0100

    Fix import of CanBeError

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup
 
-version = '0.7.1'
+version = '0.7.2'
 
 setup(
     name = 'webdriverwrapper',
--- webdriverwrapper/decorators.py
+++ webdriverwrapper/decorators.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 
-from errors import ShouldBeErrorPage, ShouldBeError
+from errors import ShouldBeErrorPage, ShouldBeError, CanBeError
 from gotopage import GoToPage, ShouldBeOnPage
