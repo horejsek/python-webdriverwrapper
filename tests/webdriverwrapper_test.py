6b74338c0389b6f26a66c9a2fca8f0e2261ef79c
Michal Horejsek <horejsekmichal@gmail.com>
2013-03-04 14:20:54 +0100

    Refactoring of WebDriverWrapper Do not decorate all methods for ensuring of webelementwrapper, just find* methods. Overwrite find* methods and catch exceptions for element's and append some useful comment.

---
+++ tests/webdriverwrapper_test.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+#  In this test is not used TestCase's aliases, because it's not target of this
+#+ test. There is testing mainly driver instance.
+#  WebdriverTestCase is used because of creating of driver.
+
+from mock import Mock, call
+import os
+
+from webdriverwrapper import WebdriverTestCase, Chrome
+from webdriverwrapper.webdriverwrapper import _WebElementWrapper
+from webdriverwrapper.exceptions import NoSuchElementException
+
+
+class WebdriverWrapperTest(WebdriverTestCase):
+    def setUp(self):
+        self.driver.go_to('http://www.google.com')
+
+    def _get_driver(self):
+        return Chrome()
+
+    def test_exception(self):
+        try:
+            self.driver.get_elm('some_non_exist_id')
+        except NoSuchElementException, e:
+            self.assertTrue(
+                e.msg and 'some_non_exist_id' in e.msg, 
+                'Exception has bad message (%s)' % e.msg,
+            )
+        except Exception, e:
+            self.fail('Excepted NoSuchElementException, but raised %s instead' % e.__class__)
+        else:
+            self.fail('NoSuchElementException not raised')
+
+    def test_returns_wrapped_element(self):
+        element = self.driver.find_element_by_id('gbqfq')
+        self.assertTrue(isinstance(element, _WebElementWrapper), 'Element instance is not WebElementWrapper')
--- webdriverwrapper/webdriverwrapper.py
+++ webdriverwrapper/webdriverwrapper.py
@@ -1,42 +1,88 @@
 # -*- coding: utf-8 -*-
 
-import inspect
-from selenium.common.exceptions import StaleElementReferenceException
+import functools
+import selenium.common.exceptions as selenium_exc
 from selenium.webdriver import *
+from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import Select, WebDriverWait
 
-from exceptions import NoSuchElementException, _create_elm_text
+from exceptions import _create_exception_msg
 import gotopage
 
 
+class _ConvertToWebelementWrapper(object):
+    def __call__(self, f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwds):
+            res = f(*args, **kwds)
+            res = self._convert_result(res)
+            return res
+        return wrapper
+
+    @classmethod
+    def _convert_result(cls, res):
+        if type(res) is WebElement:
+            res = cls._convert_into_webelementwrapper(res)
+        elif isinstance(res, (list, tuple)):
+            for index, item in enumerate(res):
+                res[index] = cls._convert_result(item)
+        return res
+
+    @classmethod
+    def _convert_into_webelementwrapper(cls, webelement):
+        try:
+            if webelement.tag_name == 'form':
+                from forms import Form
+                wrapped = Form(webelement)
+            elif webelement.tag_name == 'select':
+                wrapped = _SelectWrapper(webelement)
+            else:
+                wrapped = _WebElementWrapper(webelement)
+        except selenium_exc.StaleElementReferenceException:
+            return webelement
+        else:
+            return wrapped
+
+
 class _WebdriverBaseWrapper(object):
+    def contains_text(self, text):
+        """Does page contains `text`?"""
+        return bool(self.find_elements_by_text(text))
+
     def find_elements_by_text(self, text):
         """Find every element in page which contain `text`."""
         elms = self.find_elements_by_xpath('.//*[contains(text(), "%s")]' % text)
         return elms
 
-    def contains_text(self, text):
-        """Does page contains `text`?"""
-        return bool(self.find_elements_by_text(text))
+    def click(self, *args, **kwds):
+        if args or kwds:
+            elm = self.get_elm(*args, **kwds)
+            elm.click()
+        else:
+            super(_WebdriverBaseWrapper, self).click()
 
-    def get_elm(self, id_=None, class_name=None, name=None, tag_name=None, xpath=None, parent_id=None, parent_class_name=None, parent_name=None, parent_tag_name=None):
-        elms = self.get_elms(id_, class_name, name, tag_name, xpath, parent_id, parent_class_name, parent_name, parent_tag_name)
+    def get_elm(
+        self, 
+        id_=None, class_name=None, name=None, tag_name=None, xpath=None,
+        parent_id=None, parent_class_name=None, parent_name=None, parent_tag_name=None
+    ):
+        elms = self.get_elms(
+            id_, class_name, name, tag_name, xpath, 
+            parent_id, parent_class_name, parent_name, parent_tag_name
+        )
         if not elms:
-            raise NoSuchElementException(
-                id_,
-                class_name,
-                name,
-                tag_name,
-                xpath,
-                parent_id,
-                parent_class_name,
-                parent_name,
-                parent_tag_name,
-            )
+            raise selenium_exc.NoSuchElementException(_create_exception_msg(
+                id_, class_name, name, tag_name, xpath,
+                parent_id, parent_class_name, parent_name, parent_tag_name,
+            ))
         return elms[0]
 
-    def get_elms(self, id_=None, class_name=None, name=None, tag_name=None, xpath=None, parent_id=None, parent_class_name=None, parent_name=None, parent_tag_name=None):
+    def get_elms(
+        self,
+        id_=None, class_name=None, name=None, tag_name=None, xpath=None,
+        parent_id=None, parent_class_name=None, parent_name=None, parent_tag_name=None
+    ):
         if parent_id or parent_class_name or parent_name or parent_tag_name:
             parent = self.get_elm(parent_id, parent_class_name, parent_name, parent_tag_name)
         else:
@@ -58,12 +104,56 @@ class _WebdriverBaseWrapper(object):
         else:
             raise Exception('You must specify id or name of element on which you want to click.')
 
-    def click(self, *args, **kwds):
-        if args or kwds:
-            elm = self.get_elm(*args, **kwds)
-            elm.click()
-        else:
-            super(_WebdriverBaseWrapper, self).click()
+    def find_element(self, by=By.ID, value=None):
+        callback = self._get_seleniums_driver_class().find_element
+        return self._find_element_or_elements(callback, by, value)
+
+    def find_elements(self, by=By.ID, value=None):
+        callback = self._get_seleniums_driver_class().find_elements
+        return self._find_element_or_elements(callback, by, value)
+
+    def _get_seleniums_driver_class(self):
+        next_is_selenium_driver_class = False
+        driver_class = None
+        for cls in self.__class__.mro():
+            if next_is_selenium_driver_class:
+                driver_class = cls
+                break
+            if cls is _WebdriverBaseWrapper:
+                next_is_selenium_driver_class = True
+        if not driver_class:
+            raise Exception('WebDriver class not found')
+        return driver_class
+
+    #  Map from selenium's By class to name of params used in this wrapper. It's
+    #+ used for making helpful messages.
+    #  Commented lines are not supported.
+    _by_to_string_param_map = {
+        By.ID: 'id_',
+        By.XPATH: 'xpath',
+        #By.LINK_TEXT: 'link_text',
+        #By.PARTIAL_LINK_TEXT: 'partial_link_text',
+        By.NAME: 'name',
+        By.TAG_NAME: 'tag_name',
+        By.CLASS_NAME: 'class_name',
+        #By.CSS_SELECTOR: 'css_selector',
+    }
+
+    @_ConvertToWebelementWrapper()
+    def _find_element_or_elements(self, callback, by, value):
+        msg = _create_exception_msg(**{
+            self._by_to_string_param_map[by]: value,
+        })
+        try:
+            return callback(self, by, value)
+        except (
+            selenium_exc.NoSuchElementException,
+            selenium_exc.StaleElementReferenceException,
+            selenium_exc.InvalidElementStateException,
+            selenium_exc.ElementNotVisibleException,
+            selenium_exc.ElementNotSelectableException,
+        ), e:
+            raise e.__clas__(msg)
 
 
 class _WebdriverWrapper(_WebdriverBaseWrapper):
@@ -75,7 +165,7 @@ class _WebdriverWrapper(_WebdriverBaseWrapper):
         but appends useful message if it's not provided.
         """
         if not message:
-            message = _create_elm_text(*args, **kwds)
+            message = _create_exception_msg(*args, **kwds)
         self.wait(timeout).until(lambda driver: driver.get_elm(*args, **kwds), message=message)
 
     def wait(self, timeout=10):
@@ -105,57 +195,34 @@ class _WebElementWrapper(_WebdriverBaseWrapper, WebElement):
         return instance
 
     def __init__(self, webelement):
+        #  Nothing to do because whole __dict__ of original WebElement was 
+        #+ copied during creation of instance.
         pass
 
 
-class _SelectWrapper(Select, _WebElementWrapper):
+class _SelectWrapper(_WebElementWrapper, Select):
+    def __init__(self, webelement):
+        #  WebElementWrapper is created by coping __dict__ of WebElement instance
+        #+ in method __new__ of _WebElementWrapper. So there have to be called
+        #+ only init method of Select.
+        Select.__init__(self, webelement)
+
+
+class Chrome(_WebdriverWrapper, Chrome):
     pass
 
 
-def _webdriver_wrapper_decorator(cls):
-    for name, method in inspect.getmembers(cls, inspect.ismethod):
-        setattr(cls, name, _webelement_wrapper_decorator(method))
-    return cls
-
-
-def _webelement_wrapper_decorator(f):
-    def wrapper(*args, **kwds):
-        res = f(*args, **kwds)
-        if type(res) is WebElement:
-            try:
-                if res.tag_name == 'form':
-                    from forms import Form
-                    res = Form(res)
-                elif res.tag_name == 'select':
-                    res = _SelectWrapper(res)
-                else:
-                    res = _WebElementWrapper(res)
-            except StaleElementReferenceException:
-                pass
-        return res
-    return wrapper
-
-
-@_webdriver_wrapper_decorator
-class Chrome(Chrome, _WebdriverWrapper):
+class Firefox(_WebdriverWrapper, Firefox):
     pass
 
 
-@_webdriver_wrapper_decorator
-class Firefox(Firefox, _WebdriverWrapper):
+class Ie(_WebdriverWrapper, Ie):
     pass
 
 
-@_webdriver_wrapper_decorator
-class Ie(Ie, _WebdriverWrapper):
+class Opera(_WebdriverWrapper, Opera):
     pass
 
 
-@_webdriver_wrapper_decorator
-class Opera(Opera, _WebdriverWrapper):
-    pass
-
-
-@_webdriver_wrapper_decorator
-class Remote(Remote, _WebdriverWrapper):
+class Remote(_WebdriverWrapper, Remote):
     pass
