b7d0713ffcc3c9abe9b7b14a289c8a55784d67e0
Michal Horejsek <horejsekmichal@gmail.com>
2013-07-05 16:25:55 +0200

    Fixed imports

--- webdriverwrapper/__init__.py
+++ webdriverwrapper/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+
 from webdriverwrapper import decorators
 from webdriverwrapper import exceptions
 from webdriverwrapper import testcase
