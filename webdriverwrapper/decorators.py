e5d01523cd028418216997a7e3a4638f553873a5
Michal Horejsek <horejsekmichal@gmail.com>
2013-07-05 16:25:55 +0200

    Fixed imports

--- webdriverwrapper/decorators.py
+++ webdriverwrapper/decorators.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+
 from webdriverwrapper.errors import ShouldBeErrorPage, ShouldBeError, CanBeError
 from webdriverwrapper.info import ShouldBeInfo
 from webdriverwrapper.gotopage import GoToPage, ShouldBeOnPage
--- webdriverwrapper/fuzzytestcase.py
+++ webdriverwrapper/fuzzytestcase.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+
 import random
 from selenium.common.exceptions import WebDriverException, NoAlertPresentException
 
