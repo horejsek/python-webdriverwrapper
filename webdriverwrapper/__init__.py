43e0677658a60e2b3719093671bbb9b0a969f290
Michal Horejsek <horejsekmichal@gmail.com>
2013-07-05 13:03:42 +0200

    Explicit import in init (so editors can autocomplete)

--- webdriverwrapper/__init__.py
+++ webdriverwrapper/__init__.py
@@ -3,4 +3,17 @@
 from webdriverwrapper import decorators
 from webdriverwrapper import exceptions
 from webdriverwrapper import testcase
-from webdriverwrapper.wrapper import *
+from webdriverwrapper.wrapper import (
+    Firefox,
+    FirefoxProfile,
+    Chrome,
+    ChromeOptions,
+    Ie,
+    Opera,
+    PhantomJS,
+    Remote,
+    DesiredCapabilities,
+    ActionChains,
+    TouchActions,
+    Proxy,
+)
