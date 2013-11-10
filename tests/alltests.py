e23400be4d5f6a904a7971edbdf4cbd41d240e0c
Michal Horejsek <horejsekmichal@gmail.com>
2013-11-10 15:09:08 +0100

    If tests fails, exit with non-zero code

--- tests/alltests.py
+++ tests/alltests.py
@@ -4,4 +4,7 @@ import sys
 
 if __name__ == '__main__':
     from nose import run
-    run(argv=[sys.argv[0], '--all-modules'])
+    ok = run(argv=[sys.argv[0], '--all-modules'])
+    if not ok:
+        sys.exit(1)
+
