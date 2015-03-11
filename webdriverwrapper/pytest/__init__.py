0a043079a9e0c4182495813cf2653ddbfe4cc3b1
Jeff Johnson <jeffcjohnson@gmail.com>
2015-03-11 11:52:12 -0400

    fix imports for 'make test'

--- webdriverwrapper/pytest/__init__.py
+++ webdriverwrapper/pytest/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from conftest import pytest_report_header, pytest_runtest_makereport, set_driver_to_test_for_failed_screenshot, driver
+from .conftest import pytest_report_header, pytest_runtest_makereport, set_driver_to_test_for_failed_screenshot, driver
